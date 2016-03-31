31c71d3f25adb123e87e7b674e90ec808033bcfe
Amila17 <amila.prabandhika@bedegaming.com>
2016-03-31 14:51:47 +0100

    Committing test project.

---
+++ src/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Person Get(int id)
+        { 
+            return new Person()
+            {
+                Name = "Test",
+                Age = "12"
+            };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public Person Post([FromBody] Person person)
+        {
+            return person;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public Person Put(int id, [FromBody]Person person)
+        {
+            person.Name = "Updated Name";
+            return person;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
+/*
+Below is a valid json to call this
+
+/*    jQuery.ajax({
+            type: "PUT",
+            headers: {"Accept" : "#1#*", "Content-Type":"application/json"},
+            dataType: "json",
+            url: 'http://localhost:5412/api/person/1',
+            data: JSON.stringify({Name : "Amila", Age : "12"})
+            })*/
+
+*/
---
+++ src/WebApplication1/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
